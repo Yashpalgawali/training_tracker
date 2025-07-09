55e93a5d164b85ed838cf185f0bf70aa2fe511ac
YashpalGawali <gawaliyashpal@gmail.com>
2025-07-09 17:51:06 +0530

    09 July JWT auth implemented

--- src/main/java/com/example/demo/security/JwtAuthenticationResource.java
+++ src/main/java/com/example/demo/security/JwtAuthenticationResource.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,24 +24,25 @@ public class JwtAuthenticationResource {
 	}
 
 	@PostMapping("/authenticate")
-	public JwtResponse postMethodName(Authentication authentication) {
-		
-		
+	public JwtResponse authenticate(Authentication authentication) {
 		
 		return new JwtResponse(createToken(authentication));
 	}
 
 	private String createToken(Authentication authentication) {
 		
-		JwtClaimsSet.builder()
-						.issuer("self")
-						.issuedAt(Instant.now())
-						.expiresAt(Instant.now().plusSeconds(60 * 30))
-						.subject(authentication.getName())
-						.claim("scope", createScope(authentication))
-						;
+		var claims = JwtClaimsSet.builder().issuer("self")
+				.issuedAt(Instant.now())
+				.expiresAt(Instant.now().plusSeconds(60*30))
+				.subject(authentication.getName()).claim("scope", createScope(authentication))
+				.claim("scope", createScope(authentication)) // 👈 Custom claim: adds user_id to the payload of the token.
+				   
+				.build()
+				
+				;
+
+		return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
 		
-		return null;
 	}
 
 	private String createScope(Authentication authentication) {
