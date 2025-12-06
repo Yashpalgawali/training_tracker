d3a94020378bcb4500e2346b6e2526a3bc9c3486
Yashpal Gawali <gawaliyashpal@gmail.com>
2025-12-06 11:04:30 +0530

    06 Dec fir commit

--- src/main/java/com/example/demo/service/impl/EmployeeHistoryImpl.java
+++ src/main/java/com/example/demo/service/impl/EmployeeHistoryImpl.java
@@ -10,19 +10,15 @@ import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.EmployeeHistoryRepository;
 import com.example.demo.repository.EmployeeRepository;
 import com.example.demo.service.IEmployeeHistoryService;
-import com.example.demo.service.IEmployeeService;
+
+import lombok.RequiredArgsConstructor;
 
 @Service("emphistserv")
+@RequiredArgsConstructor
 public class EmployeeHistoryImpl implements IEmployeeHistoryService {
 
 	private final EmployeeHistoryRepository emphistrepo;
 	private final EmployeeRepository emprepo;
-
-	public EmployeeHistoryImpl(EmployeeHistoryRepository emphistrepo, EmployeeRepository emprepo) {
-		super();
-		this.emphistrepo = emphistrepo;
-		this.emprepo = emprepo;
-	}
 
 	@Override
 	public EmployeeHistory saveEmployeeHistory(EmployeeHistory history) {
@@ -39,7 +35,11 @@ public class EmployeeHistoryImpl implements IEmployeeHistoryService {
 	@Override
 	public List<EmployeeHistory> getEmployeeHistoryByEmployeeId(Long empid) {
 		Employee emp = emprepo.findById(empid).get();
-		return emphistrepo.findByEmployee(emp);
+		List<EmployeeHistory> empHist = emphistrepo.findByEmployee(emp);
+		if(empHist.size()>0) {
+			return empHist;
+		}
+		throw new ResourceNotFoundException("No History found of Employee "+emp.getEmpName());
 	}
 
 }
